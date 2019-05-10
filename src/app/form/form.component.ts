cf90bb06aa4edc581bb39ac0c6cf492d3455af39
mymuen <szymon.zyla@qnt.pl>
2019-05-10 09:29:43 +0200

    first commit

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+export class FormComponent implements OnInit {
+
+  messageForm: FormGroup;
+  submitted = false;
+  success = false;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.messageForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      message: ['', Validators.required],
+    })
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.messageForm.invalid) {
+      return;
+    }
+
+    this.success = true;
+  }
+
+
+  ngOnInit() {
+  }
+
+}
